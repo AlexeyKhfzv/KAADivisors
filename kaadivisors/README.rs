9994d2d3681eea13414fcd82fd50a7919ad7d223
Alexey <aa-khafizov@ya.ru>
2026-05-31 17:57:34 +0500

    Create README.rs

---
+++ kaadivisors/README.rs
@@ -0,0 +1,34 @@
+# KaaDivisors
+Simple CLI program for finding divisors of huge numbers
+
+Very FAST. Written in Rust.
+
+[![License](https://img.shields.io/badge/License-%20Apache%202.0-green.svg)](#)
+[![License](https://img.shields.io/badge/License-%20MIT%20-green.svg)](#)
+[![Rust](https://img.shields.io/badge/Rust-%23000000.svg?e&logo=rust&logoColor=white)](#)
+
+## Getting Started
+`cargo install kaadivisors`  
+`kaadivs`
+
+## Output Example
+```
+This program returns divisors of a number
+----------
+2 ^1
+3 ^2
+5 ^1
+3607 ^1
+3803 ^1
+----------
+Finished: 28.299µs
+```
+All divisors are PRIME NUMBERS (2, 3, 5, 3607, 3803).
+
+`3 ^2` - here `3` is a <ins>prime divisor</ins>, `^2` is a <ins>power</ins> (3<sup>2</sup> = 3 × 3).
+
+Indeed, 2 × 3<sup>2</sup> × 5 × 3607 × 3803 = 1234567890
+
+## All Commands
+* `kaadivs` or `kaadivisors`
+* `kaadivs <your_number>` (example: kaadivs 123)
